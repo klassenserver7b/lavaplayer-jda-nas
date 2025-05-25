951bffd515476793264ed916d4e8b468a0988b33
klassenserver7b <klassenserver7bwin10@gmail.com>
2025-05-26 00:47:33 +0200

    Revert "add musl support"
    This reverts commit 1c6978110e1afdef4d0ffe8bc055721641895261.

--- udp-queue-natives/udpqueue/linux/timing.c
+++ udp-queue-natives/udpqueue/linux/timing.c
@@ -1,31 +1,17 @@
-#include <errno.h>
 #include <time.h>
 
 #include "../timing.h"
 
 int64_t timing_get_nanos(void) {
-  struct timespec tv;
-
-  // CLOCK_MONOTONIC is more portable than CLOCK_REALTIME
-  // especially when considering musl libc compatibility
-  if (clock_gettime(CLOCK_MONOTONIC, &tv) != 0) {
-    // Fall back to CLOCK_REALTIME if MONOTONIC fails
-    clock_gettime(CLOCK_REALTIME, &tv);
-  }
-
-  return tv.tv_sec * 1000000000LL + tv.tv_nsec;
+	struct timespec tv;
+	clock_gettime(CLOCK_REALTIME, &tv);
+	return tv.tv_sec * 1000000000LL + tv.tv_nsec;
 }
 
 void timing_sleep(int64_t nanos) {
-  struct timespec tv = {nanos / 1000000000LL, nanos % 1000000000LL};
-
-  struct timespec rem;
-  while (nanosleep(&tv, &rem) == -1) {
-    if (errno != EINTR) {
-      // If error is not due to interrupted system call, break
-      break;
-    }
-    // Otherwise, continue sleeping for the remaining time
-    tv = rem;
-  }
+	struct timespec tv = {
+		nanos / 1000000000LL,
+		nanos % 1000000000LL
+	};
+	nanosleep(&tv, NULL);
 }
